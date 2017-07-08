2fb718cae24c00aa550b7178feb14e06b58a756d
Vladimir Ananiev <vladeto97@gmail.com>
2017-07-09 02:05:30 +0300

    Auth forms validation

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -71,11 +71,34 @@ export function login (user) {
 }
 
 function validateLogin (user) {
-  return {isValid:true} //TODO
+  if (!user || user.email.trim().length === 0) {
+    return {isValid: false, message: 'Please provide your email address.'}
+  }
+
+  if (!user || user.password.trim().length === 0) {
+    return {isValid: false, message: 'Please provide your password.'}
+  }
+
+  return {isValid:true}
 }
 
 function validateRegistration (user) {
-  //TODO: validate, return Obj {isValid: true|false, ?err:[err message]}
+  if (!user || !user.email) { //TODO: check regex
+    return {isValid: false, message: 'Please provide a correct email address.'}
+  }
+
+  if (!user || user.password.trim().length < 4) {
+    return {isValid: false, message: 'Password must have at least 4 characters.'}
+  }
+
+  if (!user || user.password !== user.confirmPassword) {
+    return {isValid: false, message: 'Passwords must match.'}
+  }
+
+  if (!user || user.name.trim().length === 0) {
+    return {isValid: false, message: 'Please provide your name.'}
+  }
+
   return {isValid: true}
 }
 
