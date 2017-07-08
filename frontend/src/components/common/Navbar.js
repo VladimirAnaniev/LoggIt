a11b62f9f3c4d780b0ab18e7b1308e33bd1ca082
Vladimir Ananiev <vladeto97@gmail.com>
2017-07-09 01:52:28 +0300

    Navbar styles

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Auth from '../../utilities/Auth'
+import styled from 'styled-components'
 
-export default function Navbar () {
+const Nav = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+`
+
+const NavItem = styled.li`
+  float: left;
+
+  a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+}
+`
+
+export default function NavBar () {
   return (
-    <div className='menu'>
-      <nav >
-        <Link to='/'>Home</Link>
-        {Auth.isUserAuthenticated() ? (
-          <div className="inline-block">
+    <Nav>
+      <NavItem><Link to='/'>Home</Link></NavItem>
 
-          </div>
-        ) : (
-          <div className="inline-block">
-            <Link to='/user/register'>Register</Link>
-            <Link to='/user/login'>Login</Link>
-          </div>
-        )}
-      </nav>
-    </div>
+      {Auth.isUserAuthenticated() ? (
+        <div className="inline-block">
+
+        </div>
+      ) : (
+        <div>
+          <NavItem><Link to='/user/register'>Register</Link></NavItem>
+          <NavItem><Link to='/user/login'>Login</Link></NavItem>
+        </div>
+      )}
+    </Nav>
   )
 
 }
